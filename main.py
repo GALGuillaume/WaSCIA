5776a781ac841a6ad0555b9070f307a4105823a6
GALGuillaume <eguillaume.gallion@gmail.com>
2023-03-06 12:41:12 +0000

    Remove debug.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # Preparationg for Pair Coding Session
 
 # STEP 1: Function to extract the needed data from netCDF file to a directory.
-def extract_data():
-    # TODO
+def extract_data(sentinel3_folder, output_folder):
+    # Gather paths to NetCDF files and metdata xml file
+
+    # Include timestamp in output_folder name
+
+    # Extract from each NetCDF file the relevant bands
+
     pass
 
-# STEP 2: Apply bias correction to data where needed.
+# STEP 2: Apply offset and fraction correction to data where needed.
 def bias_correction():
     # TODO
     pass
